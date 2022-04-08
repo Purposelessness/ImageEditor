9dadb1ca751e6d2953651950e978d333ea9e9dfa
Michael <yagodarov.misha.2003@mail.ru>
2022-04-08 18:58:28 +0300

    feat(core): added generic state machine

---
+++ QtApplication/Core/StateMachine/context.cpp
@@ -0,0 +1,25 @@
+#include "context.h"
+#include "../../logger.h"
+
+template<typename State>
+void Context<State>::addState(const QString &name, State *state) {
+    if (states.contains(name))
+        qInfo(core()) << "Context already contains" << name << ". Previous state was replaced";
+    states.insert(name, state);
+    qDebug(toolService()) << "Tool" << name << "added to hash";
+}
+
+template<typename State>
+void Context<State>::setState(const QString &name) {
+    if (!states.contains(name)) {
+        qCritical(core()) << "State" << name << "doesn't contain in context";
+        return;
+    }
+    currentState = states.value(name);
+    qInfo(core()) << "Selected" << name << "state";
+}
+
+template<typename State>
+State *Context<State>::getState() {
+    return currentState;
+}
---
+++ QtApplication/Core/StateMachine/context.h
@@ -0,0 +1,22 @@
+#ifndef IMAGEEDITOR_CONTEXT_H
+#define IMAGEEDITOR_CONTEXT_H
+
+
+#include <QHash>
+
+template<typename State>
+class Context {
+public:
+    virtual void addState(const QString &name, State *state);
+    virtual void setState(const QString &name);
+    State *getState();
+
+protected:
+    QHash<QString, State> states;
+
+private:
+    State *currentState;
+};
+
+
+#endif //IMAGEEDITOR_CONTEXT_H
