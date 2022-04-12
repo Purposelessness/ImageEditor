e44fe69d610bf1b85158796654f96fea1ff827c9
Michael <yagodarov.misha.2003@mail.ru>
2022-04-12 19:35:23 +0300

    fix(figure): figure deselection

--- QtApplication/MainWidgets/ImageViewer/imagecontainer.h
+++ QtApplication/MainWidgets/ImageViewer/imagecontainer.h
@@ -44,11 +44,16 @@ private:
 
             if (event->type() == QEvent::GraphicsSceneMouseDoubleClick) {
                 watched->setSelected(true);
+                selectedItem = watched;
+            } else if (event->type() == QEvent::GraphicsSceneMousePress && selectedItem) {
+                selectedItem->setSelected(false);
             }
             return true;
         }
         [[nodiscard]] QRectF boundingRect() const final { return QRect(); }
         [[maybe_unused]] void paint(QPainter *qPainter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr) override {}
+
+        QGraphicsItem *selectedItem = nullptr;
     } eventFilter;
 
     QGraphicsScene *scene;
